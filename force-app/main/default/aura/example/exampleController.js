bda565a804c4d75b350cc14431affa6f4c409c85
David Frudd <dfrudd@financialforce.com>
2018-09-03 16:00:16 +0100

    Example lightning component

---
+++ force-app/main/default/aura/example/exampleController.js
@@ -0,0 +1,13 @@
+({
+    onCancelButtonPressed : function(component, event, helper) {
+		helper.handleCancelButtonPress(component);
+	},
+	
+	onSearchButtonPressed : function(component, event, helper) {
+		helper.handleSearchButtonPress(component);
+	},
+
+    onSettingsButtonPressed : function(component, event, helper) {
+		helper.handleSettingsButtonPress(component);
+	}
+})
---
+++ force-app/main/default/aura/example/exampleHelper.js
@@ -0,0 +1,13 @@
+({
+	handleCancelButtonPress: function(component) {
+		console.log('Cancel button pressed');
+	},
+	
+	handleSearchButtonPress: function(component) {
+		console.log('Search button pressed');
+	},
+	
+    handleSettingsButtonPress: function(component) {
+		console.log('Settings button pressed');
+    }
+})
