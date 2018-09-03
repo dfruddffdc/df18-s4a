9e0066cbc8ab384d1a0c25a22579832690d19ef3
David Frudd <dfrudd@financialforce.com>
2018-09-03 16:14:45 +0100

    Check validity, show toasts

--- force-app/main/default/aura/example/exampleHelper.js
+++ force-app/main/default/aura/example/exampleHelper.js
@@ -1,13 +1,33 @@
 ({
 	handleCancelButtonPress: function(component) {
-		console.log('Cancel button pressed');
+		this.showToast('info', 'Cancel button pressed');
 	},
 	
 	handleSearchButtonPress: function(component) {
-		console.log('Search button pressed');
+		var allValid = component
+			.find('formField')
+			.reduce(function (validSoFar, inputCmp) {
+				inputCmp.reportValidity();
+				return validSoFar && inputCmp.checkValidity();
+			}, true);
+		
+        if (allValid) {
+			this.showToast('success', 'Searched for "' + component.get('v.searchTerm') + '"');
+        } else {
+            this.showToast('error', 'Resolve the form errors and try again.');
+		}
 	},
 	
     handleSettingsButtonPress: function(component) {
-		console.log('Settings button pressed');
-    }
+		this.showToast('info', 'Settings button pressed');
+	},
+	
+	showToast: function (type, message) {
+		var toast = $A.get("e.force:showToast");
+		toast.setParams({
+			message: message || type, // when there's no message, use type as fallback
+			type: type
+		});
+		toast.fire();
+	},
 })
